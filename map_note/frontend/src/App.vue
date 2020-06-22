4b571121077d86458cf88dd123b96b6225ef67ba
kim gi tae <1004gite@naver.com>
2020-06-23 02:49:37 +0900

    .

--- map_note/frontend/src/App.vue
+++ map_note/frontend/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <div id = "map"></div>
+
     {{msg}}
     <form @submit.prevent="submitNote">
       <label>Title</label>
@@ -37,6 +39,62 @@ export default {
     }
   },
   methods: {
+    initMap() {
+      var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 지도를 클릭했을때 클릭한 위치에 마커를 추가하도록 지도에 클릭이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+    // 클릭한 위치에 마커를 표시합니다
+    addMarker(mouseEvent.latLng);
+});
+
+// 지도에 표시된 마커 객체를 가지고 있을 배열입니다
+var markers = [];
+
+// 마커 하나를 지도위에 표시합니다
+addMarker(new kakao.maps.LatLng(33.450701, 126.570667));
+
+// 마커를 생성하고 지도위에 표시하는 함수입니다
+function addMarker(position) {
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: position
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    // 생성된 마커를 배열에 추가합니다
+    markers.push(marker);
+}
+
+// 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
+function setMarkers(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// "마커 보이기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에 표시하는 함수입니다
+function showMarkers() {
+    setMarkers(map)
+}
+
+// "마커 감추기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에서 삭제하는 함수입니다
+function hideMarkers() {
+    setMarkers(null);
+}
+
+        },
+
+
     submitNote () {
       api.fetchNotes('post', null, this.formData).then(res => {
         this.msg = 'Saved'
@@ -54,12 +112,23 @@ export default {
       })
     }
   },
-  mounted () {
-    // fetch all notes once component is mounted
-    this.fetchAllNotes()
-  }
+
+    mounted() {
+      const script = document.createElement('script');
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=9e9c50ee07c661cd80d3e59130f4703a';
+      document.head.appendChild(script);
+      this.fetchAllNotes()
+    }
 }
 </script>
+<style>
+#map {
+  text-align: center;
+  width: 600px;
+  height: 500px;
+}
+</style>
 
 <style lang="scss">
 #app {
@@ -109,4 +178,3 @@ a {
     padding: 6px 10px;
   }
 </style>
-
