6a6c912288f0e7f4adbbbc02289ae618dd393669
kim gi tae <1004gite@naver.com>
2020-06-24 00:01:58 +0900

    .

--- map_note/frontend/src/App.vue
+++ map_note/frontend/src/App.vue
@@ -59,9 +59,22 @@
         var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
         // 지도를 클릭했을때 클릭한 위치에 마커를 추가하도록 지도에 클릭이벤트를 등록합니다
         // 마커를 중앙에 생성합니다, 마커가 중복되서 찍히지 않기 위해 addmarker 함수밖으로 뺴줍니다.
-        var marker = new kakao.maps.Marker(
+        var marker = new kakao.maps.Marker();
 
-        )
+
+                  //컨트롤러를 생성하는 함수 입니다.
+          // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+          var mapTypeControl = new kakao.maps.MapTypeControl();
+
+          // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+          // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+          map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+          // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+          var zoomControl = new kakao.maps.ZoomControl();
+          map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+
 
         // 마커가 지도 위에 표시되도록 설정합니다
         marker.setMap(map);
@@ -79,7 +92,6 @@
           var message = '현재 핑의 위치입니다.<br/> 위도 : ' + latlng.getLat() +  '<br/>경도 : ' + latlng.getLng() + '<br/>--------------------------------------------<br/>';
           var resultDiv = document.getElementById('clickLatlng');
           resultDiv.innerHTML = message;
-
 
         });
         // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
---
+++ map_note/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\1004g\desktop\new\oss2020\map_note\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
