fde21a50d9cbb7455d31e0386fa2cef45481d5fc
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-06-24 19:46:12 +0900

    backend_delete

--- map_note/vuenote/models.py
+++ map_note/vuenote/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Note(models.Model):
+class Note(models.Model):   # Note 테이블의 필드 (메모, 위도, 경도, 생성시각)
     memo = models.CharField(max_length=255, unique=False)
     latitudes = models.FloatField(max_length=10)
     longitudes = models.FloatField(max_length=10)
--- map_note/vuenote/views.py
+++ map_note/vuenote/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -12,16 +11,25 @@ from vuenote.serializers import NoteSerializer
 
 class NoteView(APIView):
     @staticmethod
-    def post(request):
-        serializer = NoteSerializer(data=request.data)
+    def post(request):  # post request를 처리하는 함수
+        serializer = NoteSerializer(data=request.data)  # 받아온 데이터를 serializer에 저장
         if serializer.is_valid():
-            serializer.save()
-        return HttpResponse("good")
+            serializer.save()   # 데이터베이스에 저장
+        return HttpResponse("save")
 
     @staticmethod
-    def get(request):
-        info = Note.objects.all()
-        serializer = NoteSerializer(info, many=True)
-        return JsonResponse(serializer.data, safe=False)
+    def get(request):   # get request를 처리하는 함수
+        info = Note.objects.all()   # 가지고있는 모든 데이터를 데이터베이스에서 불러와 info에 저장
+        serializer = NoteSerializer(info, many=True)    # serializer로 만든다
+        return JsonResponse(serializer.data, safe=False)    # Json 포맷으로 으로 response
 
+    @staticmethod
+    def delete(request):    # delete request를 처리하는 함수
+        deletemodel = request.data  # 받아온 데이터를 deletemodel 에 저장한다
+        d_memo = str(deletemodel['memo'])
+        d_latitudes = float(deletemodel['latitudes'])
+        d_longitudes = float(deletemodel['longitudes'])
+        queryset = Note.objects.filter(memo=d_memo, latitudes=d_latitudes, longitudes=d_longitudes)   # 테이블에서 필터를 통해 조회한후 queryset에 저장
+        queryset.delete()   # 해당 데이터를 데이터베이스에서 삭제
 
+        return HttpResponse("delete")
